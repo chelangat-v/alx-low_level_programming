d3a73b1e8c02d5bda35f2e9259cf385e7571fb7c
chelangatv <vickychelangat788@gmail.com>
2023-09-25 15:31:42 -0700

    Singly Linked Lists

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * Author: Vicky Chelangat
+ *
+ * free_listint - frees a list
+ *
+ * @head: pointer
+ *
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * Author: Vicky Chelangat
+ *
+ * free_listint2 - frees a list but head is null
+ *
+ * @head: linked list and head
+ *
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		tmp = *head;
+		free(*head);
+		*head = tmp->next;
+	}
+	*head = NULL;
+}
